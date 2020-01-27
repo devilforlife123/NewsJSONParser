c1bc462c98eb0432a5c64ebbc60775957f4457ec
devilforlife123 <surajpoudelg@gmail.com>
2020-01-27 22:17:56 +1100

    Initial Commit

---
+++ NewsJSONParser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsJSONParser
+//
+//  Created by suraj poudel on 27/1/20.
+//  Copyright © 2020 suraj poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
