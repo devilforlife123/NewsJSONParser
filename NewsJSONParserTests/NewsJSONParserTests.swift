e57f7c32883a239eb733c031007181c9608909a7
devilforlife123 <surajpoudelg@gmail.com>
2020-01-28 06:01:05 +1100

    add unit testing function

--- NewsJSONParserTests/NewsJSONParserTests.swift
+++ NewsJSONParserTests/NewsJSONParserTests.swift
@@ -11,13 +11,45 @@ import XCTest
 
 class NewsJSONParserTests: XCTestCase {
 
+      var sut: URLSession!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = URLSession(configuration: .default)
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+         sut = nil
     }
+    
+    func testValidGetsHTTPStatusCode200(){
+        
+    
+        // given
+        let url = URL(string: API.urlAddress)
+           // 1
+           let promise = expectation(description: "Status code: 200")
+           
+           // when
+           let dataTask = sut.dataTask(with: url!) { data, response, error in
+             // then
+             if let error = error {
+               XCTFail("Error: \(error.localizedDescription)")
+               return
+             } else if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+               if statusCode == 200 {
+                 // 2
+                 promise.fulfill()
+               } else {
+                 XCTFail("Status code: \(statusCode)")
+               }
+             }
+           }
+           dataTask.resume()
+           // 3
+           wait(for: [promise], timeout: 5)
+    }
+    
 
     func testExample() {
         // This is an example of a functional test case.
